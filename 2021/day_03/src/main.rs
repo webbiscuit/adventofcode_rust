1a5744c1ccd2ad5dacdbb46daac38f2e1dd35b7a
Daniel <daniel@webbiscuit.co.uk>
2021-12-02 19:58:50 +0000

    Prep day 3

---
+++ 2021/day_03/src/main.rs
@@ -0,0 +1,23 @@
+use std::error::Error;
+use std::io::{self, prelude::*};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let stdin = io::stdin();
+    let lines = stdin.lock().lines();
+
+    let parsed: Result<Vec<u32>, Box<dyn Error>> = lines.map(|line| Ok(line?.parse::<u32>()?)).collect();
+
+    match parsed {
+        Ok(p) => {
+            for n in p {
+                println!("{}", n);
+            }
+        },
+        Err(e) => {
+            eprintln!("Error parsing file: {}", e);
+            return Err(e);
+        }
+    }
+
+    Ok(())
+}
