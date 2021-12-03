5fa1bfb4daf0720e9eeac3505cf5912d45b40260
Daniel <daniel@webbiscuit.co.uk>
2021-12-03 18:31:07 +0000

    Tests pass (only for 5 bits)

--- 2021/day_03/src/main.rs
+++ 2021/day_03/src/main.rs
@@ -1,14 +1,20 @@
-use std::error::Error;
 use std::io::{self, prelude::*};
+use std::num::ParseIntError;
 
-fn calculate_power_consumption(report_items: &[u16]) -> u16 {
-    let mut result = 0u16;
+fn calculate_power_consumption(report_items: &[u16]) -> u32 {
+    let max = report_items.iter().max().unwrap();
 
-    for n in 0..12 {
+    let max_ones = u16::BITS - max.leading_zeros();
+
+    // let max_ones = max.count_ones() as u32;
+
+    let mut result = 0u32;
+
+    for n in 0..=max_ones {
         let mut hi_bit_count = 0;
 
         for i in report_items {
-            if i & 1 << n == 1 << n {
+            if (i & 1 << n) == (1 << n) {
                 hi_bit_count += 1;
             }
 
@@ -18,37 +24,31 @@ fn calculate_power_consumption(report_items: &[u16]) -> u16 {
         }
     }
 
-    let mask = 0b111111111111; 
+    // let mask = 1 << max_ones; //0b1111111111111 & max_ones;
+    let mask = 0b11111;
 
-    println!("x: {:0>12b}", result);
-    println!("x: {:0>12b}", result ^ mask);
+    // println!("max: {:0b}", max);
+    // println!("max: {}", max_ones);
+    // println!("mask: {:0b}", mask);
+    // println!("x: {:0>12b}", result);
+    // println!("x: {:0>12b}", result ^ mask);
 
     result * (result ^ mask)
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), ParseIntError> {
     let stdin = io::stdin();
     let lines = stdin.lock().lines();
 
-    let parsed: Result<Vec<u16>, Box<dyn Error>> =
-        lines.map(|line| u16::from_str_radix(&line.unwrap(), 2)).collect();
-
-    // let parsed: Result<Vec<u16>, Box<dyn Error>> = lines
-    //     .map(|line| u16::from_str(&line.unwrap()))
-    //     .collect();
-
-    // let bin_idx = "01110011001";
-    // let intval = isize::from_str_radix(bin_idx, 2).unwrap();
-    // println!("{}", intval);
-
-    // let parsed: Result<Vec<u16>, Box<dyn Error>> = lines.map(|line| Ok(line?.parse::<u16>()?)).collect();
+    let parsed: Result<Vec<u16>, ParseIntError> = lines
+        .map(|line| u16::from_str_radix(&line.unwrap(), 2))
+        .collect();
 
     match parsed {
-        Ok(p) => {
-            let power = calculate_power_consumption(&vec![0b000000000100, 0b000000000110, 0b000000001110]);
+        Ok(report) => {
+            let power = calculate_power_consumption(&report);
 
             println!("Power consumption of the submarine: {}", power);
-            
         }
         Err(e) => {
             eprintln!("Error parsing file: {}", e);
@@ -61,7 +61,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 #[test]
 fn test_calculate_power_consumption() {
-    let count = calculate_power_consumption(&vec![0b000000000100, 0b000000000110, 0b000000001110]);
+    let count = calculate_power_consumption(&vec![0b00100, 0b11110, 0b10110]);
 
-    assert_eq!(count, 0b000000000110 * 0b111111111001);
+    assert_eq!(count, 0b10110 * 0b01001);
 }
--- 2021/day_03/tests/test_example.rs
+++ 2021/day_03/tests/test_example.rs
@@ -14,7 +14,7 @@ fn test_example() -> Result<(), Box<dyn std::error::Error>> {
     file.read_to_string(&mut contents)?;
 
     let assert = cmd.write_stdin(contents).assert();
-    assert.stdout("Hello world!");
+    assert.stdout("Power consumption of the submarine: 198\n");
 
     Ok(())
 }
