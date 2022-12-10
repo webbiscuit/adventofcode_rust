a136dd41791249f1dff5492e95cc76440352d4bd
Daniel <daniel@webbiscuit.co.uk>
2022-12-10 10:07:53 +0000

    Improve base day

--- 2022/day_0x/tests/test_example.rs
+++ 2022/day_0x/tests/test_example.rs
@@ -3,15 +3,20 @@ use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
 
+fn read_file(filename: &str) -> String {
+    let mut file_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    file_path.push(filename);
+    let mut file = File::open(file_path).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    contents
+}
+
 #[test]
 fn test_example() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("day_0x")?;
 
-    let mut file_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    file_path.push("example.txt");
-    let mut file = File::open(file_path)?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
+    let contents = read_file("example.txt");
 
     let assert = cmd.write_stdin(contents).assert();
     assert.stdout("Hello world!");
