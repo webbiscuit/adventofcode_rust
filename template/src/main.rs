4414977aff486602334f8ea1c2014dfda624a4fd
Daniel <daniel@webbiscuit.co.uk>
2023-11-25 10:49:02 +0000

    Support data dir locations

--- template/src/main.rs
+++ template/src/main.rs
@@ -11,6 +11,10 @@ struct Args {
     #[arg(short, long, required = true)]
     out_dir: String,
 
+    /// Source (data) directory
+    #[arg(short, long, required = true, default_value = "data")]
+    source_dir: String,
+
     /// Day number
     #[arg(short, long, required = true)]
     day: u8,
@@ -21,8 +25,9 @@ fn main() -> std::io::Result<()> {
 
     let day = args.day;
     let out_folder = args.out_dir;
+    let data_folder = args.source_dir;
 
-    let template_path = "data/day_0x";
+    let template_path = format!("{data_folder}/day_0x");
     let out_dir = format!("{out_folder}/day_{:02}", day);
     let src_dir = "src";
     let test_dir = "tests";
@@ -30,26 +35,31 @@ fn main() -> std::io::Result<()> {
     fs::create_dir_all(&format!("{out_dir}/{src_dir}")).expect("Unable to create directories");
     fs::create_dir_all(&format!("{out_dir}/{test_dir}")).expect("Unable to create directories");
 
-    create_file(&out_dir, template_path, "src/main.rs", None);
+    create_file(&out_dir, &template_path, "src/main.rs", None);
 
     let day_replace_fn =
         move |text: &String| -> String { text.replace("day_0x", &format!("day_{:02}", day)) };
     create_file(
         &out_dir,
-        template_path,
+        &template_path,
         "tests/test_example.rs",
         Some(&day_replace_fn),
     );
 
-    create_file(&out_dir, template_path, "example.txt", None);
-    create_file(&out_dir, template_path, "input.txt", None);
-    create_file(&out_dir, template_path, "Cargo.toml", Some(&day_replace_fn));
+    create_file(&out_dir, &template_path, "example.txt", None);
+    create_file(&out_dir, &template_path, "input.txt", None);
+    create_file(
+        &out_dir,
+        &template_path,
+        "Cargo.toml",
+        Some(&day_replace_fn),
+    );
 
     let readme_day_replace_fn =
         move |text: &String| -> String { text.replace("Day X", &format!("Day {}", day)) };
     create_file(
         &out_dir,
-        template_path,
+        &template_path,
         "readme.md",
         Some(&readme_day_replace_fn),
     );
