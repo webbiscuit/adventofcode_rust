3c3c569588e98ca77a9671a744e7a9e842d7c34c
Daniel <daniel@webbiscuit.co.uk>
2023-12-01 13:00:51 +0000

    Add line parsing to template

--- template/data/day_0x/src/main.rs
+++ template/data/day_0x/src/main.rs
@@ -1,4 +1,9 @@
+use std::io::{self, prelude::*};
+
 fn main() -> std::io::Result<()> {
+    let stdin = io::stdin();
+    let lines: Vec<String> = stdin.lock().lines().map(|l| l.unwrap()).collect();
+
     println!("Hello, world!");
 
     Ok(())
