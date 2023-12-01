3f482d88bcab9c1ef5b523fe5c76cfbc70283367
Daniel <daniel@webbiscuit.co.uk>
2023-12-01 12:33:55 +0000

    Regex version

--- 2023/day_01/readme.md
+++ 2023/day_01/readme.md
@@ -8,3 +8,5 @@ Seems to be an okay start...fetching the numbers out of text and finding the fir
 
 Seems to be a lot harder unless I'm being thick! I used string replacing to replace "one" for 1 etc, except it needs to happen for the first and last because of things like "eightwo", so ordering is also important. This should also be 82 and not 88 as I was initially doing, as in the numbers are not consuming the text.
 Hopefully not a sign of things to come because I won't get far this year if these are already tricky!
+
+The regex version is a lot easier to understand, maybe I should have started with that.
--- 2023/day_01/src/main.rs
+++ 2023/day_01/src/main.rs
@@ -1,5 +1,7 @@
 use std::io::{self, prelude::*};
 
+use fancy_regex::Regex;
+
 fn find_calibration_value(line: &str) -> u32 {
     let digits: Vec<u32> = line.chars().filter_map(|c| c.to_digit(10)).collect();
     let first = digits.first().unwrap_or(&0);
@@ -24,46 +26,56 @@ fn number_word_to_number(word: &str) -> Option<u8> {
 }
 
 fn find_calibration_value_using_written_numbers_too(line: &str) -> u32 {
-    let mut converted_line = line.to_string();
-    let words_to_find = [
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    ];
-
-    // Sort by order these thing appear
-    let found_order = words_to_find.map(|w| (w, converted_line.find(w)));
-    let found_order = found_order
-        .iter()
-        .filter(|(_, ix)| ix.is_some())
-        .map(|(s, ix)| (s, ix.unwrap()))
-        .collect::<Vec<_>>();
-
-    if !found_order.is_empty() {
-        let min = found_order.iter().min_by_key(|(_, ix)| ix).unwrap();
-        let word = min.0.to_string();
-        let digit = number_word_to_number(&word).unwrap().to_string();
-
-        converted_line.insert(min.1, digit.chars().next().unwrap());
-    }
-
-    let found_order = words_to_find.map(|w| (w, converted_line.rfind(w)));
-    let found_order = found_order
-        .iter()
-        .filter(|(_, ix)| ix.is_some())
-        .map(|(s, ix)| (s, ix.unwrap()))
-        .collect::<Vec<_>>();
-
-    if !found_order.is_empty() {
-        let max = found_order.iter().max_by_key(|(_, ix)| ix).unwrap();
-        let word = max.0.to_string();
-        let digit = number_word_to_number(&word).unwrap().to_string();
-
-        converted_line.insert(max.1 + 1, digit.chars().next().unwrap());
-    }
-
-    // println!("Converted line: {} to {}", line, converted_line);
+    let re = Regex::new(r"(?=(one|two|three|four|five|six|seven|eight|nine))").unwrap();
+    let converted_line = re.replace_all(line, |caps: &fancy_regex::Captures| {
+        let capture = &caps[1];
+        number_word_to_number(capture).unwrap().to_string()
+    });
 
     find_calibration_value(&converted_line)
 }
+
+// fn find_calibration_value_using_written_numbers_too(line: &str) -> u32 {
+//     let mut converted_line = line.to_string();
+//     let words_to_find = [
+//         "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+//     ];
+
+//     // Sort by order these thing appear
+//     let found_order = words_to_find.map(|w| (w, converted_line.find(w)));
+//     let found_order = found_order
+//         .iter()
+//         .filter(|(_, ix)| ix.is_some())
+//         .map(|(s, ix)| (s, ix.unwrap()))
+//         .collect::<Vec<_>>();
+
+//     if !found_order.is_empty() {
+//         let min = found_order.iter().min_by_key(|(_, ix)| ix).unwrap();
+//         let word = min.0.to_string();
+//         let digit = number_word_to_number(&word).unwrap().to_string();
+
+//         converted_line.insert(min.1, digit.chars().next().unwrap());
+//     }
+
+//     let found_order = words_to_find.map(|w| (w, converted_line.rfind(w)));
+//     let found_order = found_order
+//         .iter()
+//         .filter(|(_, ix)| ix.is_some())
+//         .map(|(s, ix)| (s, ix.unwrap()))
+//         .collect::<Vec<_>>();
+
+//     if !found_order.is_empty() {
+//         let max = found_order.iter().max_by_key(|(_, ix)| ix).unwrap();
+//         let word = max.0.to_string();
+//         let digit = number_word_to_number(&word).unwrap().to_string();
+
+//         converted_line.insert(max.1 + 1, digit.chars().next().unwrap());
+//     }
+
+//     // println!("Converted line: {} to {}", line, converted_line);
+
+//     find_calibration_value(&converted_line)
+// }
 
 fn main() -> std::io::Result<()> {
     let stdin = io::stdin();
