5ff8fb3285b2fb6dfa3718e1e7455d9b0eabdd5d
Daniel <daniel@webbiscuit.co.uk>
2024-12-21 14:05:12 +0000

    Day 21 Part 1 Works for input

--- 2024/day_21/src/main.rs
+++ 2024/day_21/src/main.rs
@@ -40,7 +40,9 @@ impl Keypad {
             .unwrap();
         let gap_row = gap_ix / self.width;
 
-        let has_gap_on_row = gap_row == from_pos.1;
+        let has_gap_on_row = gap_row == from_pos.1 || gap_row == to_pos.1;
+
+        let is_two_spaces_away = (from_pos.0 as isize - to_pos.0 as isize).abs() == 2;
 
         if has_gap_on_row {
             if (to_pos.1 as isize) - (from_pos.1 as isize) < 0 {
@@ -152,8 +154,8 @@ fn calculate_final_sequence(code: &[char]) -> Vec<char> {
 
     let seq = keypad.full_route_to_code('A', &code);
 
-    // let seq_string: String = seq.iter().collect();
-    // println!("Seq {}", seq_string);
+    let seq_string: String = seq.iter().collect();
+    println!("Seq {}", seq_string);
 
     let direction_pad = Keypad::new(
         vec![None, Some('^'), Some('A'), Some('<'), Some('v'), Some('>')],
@@ -163,13 +165,13 @@ fn calculate_final_sequence(code: &[char]) -> Vec<char> {
 
     let seq2 = direction_pad.full_route_to_code('A', &seq);
 
-    // let seq_string: String = seq2.iter().collect();
-    // println!("Seq {}", seq_string);
+    let seq_string: String = seq2.iter().collect();
+    println!("Seq {}", seq_string);
 
     let seq3 = direction_pad.full_route_to_code('A', &seq2);
 
-    // let seq_string: String = seq3.iter().collect();
-    // println!("Seq {}", seq_string);
+    let seq_string: String = seq3.iter().collect();
+    println!("Seq {}", seq_string);
 
     seq3
 }
@@ -197,11 +199,12 @@ fn main() -> std::io::Result<()> {
     let result = calculate_complexity(&codes);
     println!("Sum of complexities is {}", result);
 
-    // let direction_pad = Keypad::new(
-    //     vec![None, Some('^'), Some('A'), Some('<'), Some('v'), Some('>')],
-    //     3,
-    //     2,
-    // );
+    let direction_pad = Keypad::new(
+        vec![None, Some('^'), Some('A'), Some('<'), Some('v'), Some('>')],
+        3,
+        2,
+    );
+    let seq = calculate_final_sequence(&"1".chars().collect::<Vec<_>>());
     // let rev1 = direction_pad.reverse_sequence(
     //     'A',
     //     &"<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A"
@@ -209,8 +212,8 @@ fn main() -> std::io::Result<()> {
     //         .collect::<Vec<_>>(),
     // );
 
-    // let seq_string: String = rev1.iter().collect();
-    // println!("Rev {}", seq_string);
+    let seq_string: String = seq.iter().collect();
+    println!("Rev {}", seq_string);
 
     // let rev2 = direction_pad.reverse_sequence('A', &rev1);
 
@@ -221,3 +224,5 @@ fn main() -> std::io::Result<()> {
 }
 
 // < 235218
+// < 226566
+// < 233106
